1fdc456d129de9e2b8113ee7724b53af3b8a3c7d
Evhen Nazarenko <Evgen1215@ukr.net>
2024-10-21 09:28:50 +0300

    laba 7 done

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,13 +1,27 @@
-"Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
-"Поверніть дані користувачів у форматі масиву"
-"Дані мають включати такі поля, як id та name."
+"Ваша функція повинна робити GET-запит до вказаного URL і отримати дані.";
+"Поверніть дані користувачів у форматі масиву";
+"Дані мають включати такі поля, як id та name.";
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+"https://jsonplaceholder.typicode.com/users - адреса куди робити запит";
 
-function fetchUsers() {
-  // Ваш код
+async function fetchUsers() {
+   try {
+      const response = await fetch(
+         `https://jsonplaceholder.typicode.com/users`
+      );
+      const users = await response.json();
+      const persons = users.map((user) => ({
+         ...user,
+         id: user.id,
+         name: user.name,
+      }));
+      return persons;
+   } catch (e) {
+      console.log(e);
+      return [];
+   }
 }
 
-console.log(fetchUsers())
+console.log(fetchUsers());
 
 module.exports = fetchUsers;
--- tasks/task2.js
+++ tasks/task2.js
@@ -1,16 +1,31 @@
-"Ваш код повинен зробити POST-запит до вказаного URL."
-"Для створення нового користувача, передайте в запит наступні дані:"
-"name: ваше ім’я"
-"email: ваш email"
-"Поверніть відповідь від сервера після створення користувача."
+"Ваш код повинен зробити POST-запит до вказаного URL.";
+"Для створення нового користувача, передайте в запит наступні дані:";
+"name: ваше ім’я";
+"email: ваш email";
+"Поверніть відповідь від сервера після створення користувача.";
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
+"https://jsonplaceholder.typicode.com/users - адреса куди робити запит";
 
-
-function createUser(user) {
-  // Ваш код
+async function createUser(user) {
+   try {
+      const response = await fetch(
+         "https://jsonplaceholder.typicode.com/users",
+         {
+            method: "POST",
+            headers: {
+               "Content-Type": "application/json", // Вказуємо, що передаємо дані у форматі JSON
+            },
+            body: JSON.stringify(user),
+         }
+      );
+      const data = await response.json();
+      return data;
+   } catch (e) {
+      console.log(e);
+      return null;
+   }
 }
 
-console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
+console.log(createUser({ name: "Sam", email: "fjsnfkjns2342@gmail.com" }));
 
 module.exports = createUser;
